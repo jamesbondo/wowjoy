89ce416c30db1d3eb50d7fa5915af9149f00f9ba
quxiao <xiao.qu@wowjoy.cn>
2017-01-05 14:29:57 +0800

    synch

--- 3form/server.js
+++ 3form/server.js
@@ -10,10 +10,11 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 // app.use(express.static('public'))
 // app.use(express.static('files'))
+app.use('/static',express.static('files'))
 // app.use('/static', express.static('public'))
 // app.use(express.static(path.join(__dirname, 'public')))
 // app.use('/public',express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, '')))
+// app.use(express.static(path.join(__dirname, '')))
 
 
 app.get('/', function (req, res) {
@@ -28,10 +29,10 @@ app.get('/:id', function(req, res) {
 // })
 app.post('/test',function(req,res){
   console.log(req.body)
-  res.send('Got a POST request')
+  res.send(req.body)
 })
 
 
 app.listen(3001, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3001!')
 })
---
+++ 5autotesting/conf.js
@@ -0,0 +1,6 @@
+// conf.js
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:3500/',
+  specs: ['spec.js']
+}
